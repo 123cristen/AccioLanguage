4c1284613c7586fab335ab3d0f014019b95fdd53
Sunnie So <sunnieso3741@ucla.edu>
2016-04-30 15:21:17 -0700

    input done

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 #flasky.py
 import main
+from keyphrases import *
 
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,8 +21,12 @@ def language():
 
 @app.route("/keyphrases")
 def keyphrases():
+  input_texts = '{"documents":[{"id":"1","text":"hola como estas?"},{"id":"2","text":"was ist das genau?"},{"id":"three","text":"hello my world"},]}'
+
+  # result=keyPhrases_setup(input_texts)
   return render_template(
     'keyphrases.html',
+    # body=result,
     title='AccioLanguage | Key Phrases',
   )
 
@@ -32,5 +37,19 @@ def sentiment():
     title='AccioLanguage | Sentiment Detection',
   )
 
+
+@app.route('/answers', methods=['POST'])
+def key_report():
+    text = request.form['text']
+    text = text.upper()
+    output = get_keyPhrases(text)
+    # return output
+    return render_template(
+      'keyreport.html',
+      title='AccioLanguage | Key Phrases | Report',
+      answers= output,
+    )
+
 if __name__ == "__main__":
+    app.debug=True
     app.run()
